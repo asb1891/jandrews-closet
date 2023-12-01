facc782cc11b7e29db0d2179f1549197ca4ddd48
Jason Phillips <jasonanthonyp@gmail.com>
2023-12-01 10:21:33 -0500

    pretty much done

--- Client/src/ClothingForm.js
+++ Client/src/ClothingForm.js
@@ -21,15 +21,13 @@ function ClothingForm({ onAddClothes }) {
 
     return (
         <div className="ui-segment">
-          <form className="ui form" onSubmit={handleSubmit}>
+          <form className="ui-form" onSubmit={handleSubmit}>
             <div className="inline fields">
               <input type="text" name="designer-name" placeholder="name" value={formData.designer_name} onChange={handleChange}/>
               <input type="text" name="image" placeholder="image-url" value={formData.image_url} onChange={handleChange}/>
               <input type="text" name="description" placeholder="description" value={formData.description} onChange={handleChange}/>
+                <button className="ui button" type="submit">Add To Inventory</button>
             </div>
-            <button className="ui button" type="submit">
-              Add To Inventory
-            </button>
           </form>
         </div>
       );
--- Client/src/Welcome.js
+++ Client/src/Welcome.js
@@ -3,6 +3,7 @@ import React from "react";
 function Welcome() {
     return (
         <div className="welcome">
+            <img className="img" src="https://cdn.discordapp.com/attachments/1153364432458616965/1179915265090261122/family_crest_black_and_white.png?ex=657b84b8&is=65690fb8&hm=3470ba27fa1f7318a95230afa1fb75c66552b84b3c2a1bb3681f708e9fa2cbbf&" alt="placeholder" />
             <h1 className="centered-heading">Welcome To Jandrew's Closet</h1>
         </div>
     )
